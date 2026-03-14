e7e04bdd9f78c592cb70661b0fc5aaf0f8a8842b
Nikhil Kumar <144585007+NikhilKumarPujari@users.noreply.github.com>
2026-03-15 01:47:25 +0530

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,8 +1,28 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "devops-cluster"
-  cluster_version = "1.27"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.25.0"
+
+  name    = "devops-cluster"
+  version = "1.27"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
+
+  node_groups = {
+    devops_nodes = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+      instance_type    = "t3.medium"
+    }
+  }
+}
+
+# Fetch EKS authentication token
+data "aws_eks_cluster" "eks" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "eks" {
+  name = module.eks.cluster_id
 }
