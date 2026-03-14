6f08e293d1aaccbebc390483885468c26826d6db
Nikhil Kumar <144585007+NikhilKumarPujari@users.noreply.github.com>
2026-03-15 01:46:35 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,9 @@
 provider "aws" {
   region = "ap-south-1"
 }
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
